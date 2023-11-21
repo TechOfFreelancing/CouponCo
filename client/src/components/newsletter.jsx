fb6b08b26dcc05cd50a59e6c3675005cf60fbc06
harshal255 <200280107119@ldceahm.gujgov.edu.in>
2023-11-21 22:59:04 +0530

    nextletter-MailchimpSubscribe ✨

--- client/src/components/newsletter.jsx
+++ client/src/components/newsletter.jsx
@@ -1,5 +1,38 @@
 import { BsCalendarCheck } from 'react-icons/bs'
 import { HiOutlineHandRaised } from 'react-icons/hi2'
+import MailchimpSubscribe from 'react-mailchimp-subscribe';
+
+const CustomForm = ({ status, message, onValidated }) => {
+    let email;
+
+    const submit = () =>
+        email &&
+        email.value.indexOf('@') > -1 &&
+        onValidated({
+            EMAIL: email.value,
+        });
+
+    return (
+        <div className="mt-6 flex max-w-md gap-x-4">
+            <input
+                ref={(node) => (email = node)}
+                type="email"
+                placeholder="Your email"
+                className="min-w-0 flex-auto rounded-md border-0 px-3.5 py-2 !text-black shadow-sm outline-none sm:text-sm sm:leading-6"
+            />
+            <button
+                type="submit"
+                onClick={submit}
+                className="flex-none rounded-md bg-purple-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-purple-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-500"
+            >
+                Subscribe
+            </button>
+            {status === 'sending' && <div style={{ color: 'blue' }}>Subscribing...</div>}
+            {status === 'error' && <div style={{ color: 'red' }} dangerouslySetInnerHTML={{ __html: message }} />}
+            {status === 'success' && <div style={{ color: 'green' }}>Subscribed!</div>}
+        </div>
+    );
+};
 
 export default function NewsLetter() {
     return (
@@ -12,7 +45,7 @@ export default function NewsLetter() {
                             Nostrud amet eu ullamco nisi aute in ad minim nostrud adipisicing velit quis. Duis tempor incididunt
                             dolore.
                         </p>
-                        <div className="mt-6 flex max-w-md gap-x-4">
+                        {/* <div className="mt-6 flex max-w-md gap-x-4">
                             <label htmlFor="email-address" className="sr-only">
                                 Email address
                             </label>
@@ -31,7 +64,17 @@ export default function NewsLetter() {
                             >
                                 Subscribe
                             </button>
-                        </div>
+                        </div> */}
+                        <MailchimpSubscribe
+                            url={import.meta.env.VITE_PUBLIC_MAILCHIMP_URL}
+                            render={({ subscribe, status, message }) => (
+                                <CustomForm
+                                    status={status}
+                                    message={message}
+                                    onValidated={(formData) => subscribe(formData)}
+                                />
+                            )}
+                        />
                     </div>
                     <dl className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
                         <div className="flex flex-col items-start">
@@ -55,7 +98,7 @@ export default function NewsLetter() {
                     </dl>
                 </div>
             </div>
-            
+
         </div>
     )
 }
