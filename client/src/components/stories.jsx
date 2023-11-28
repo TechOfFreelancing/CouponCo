216395b61c0b6575486402d43ab58a77be9a5dd2
Ridham <115993280+rythm01@users.noreply.github.com>
2023-11-28 16:27:58 +0530

    Few More changes

--- client/src/components/stories.jsx
+++ client/src/components/stories.jsx
@@ -68,11 +68,14 @@ const Stories = () => {
                         animate="visible"
                         transition={{ delay: index * 0.25, ease: "easeInOut", duration: 0.5 }} key={index} className="flex flex-col gap-2 h-[10rem] lg:h-[15rem] cursor-pointer group items-center justify-center hover:scale-125 duration-300">
                         <div style={{ backgroundColor: `${ele.background}` }} className="h-[5rem] w-[5rem] lg:h-[10rem] lg:w-[10rem] p-5 rounded-full flex items-center justify-center shadow-inner border overflow-hidden">
-                            <img src={images[index]} alt={`Logo ${index}`} onClick={() => {
-                                navigate(
-                                    `/Stores/${ele.name}`, { state: { sId: ele.storeId } }
-                                )
-                            }} className="h-auto w-auto max-h-full max-w-full cursor-pointer rounded-full" />
+                            <img
+                                src={images[index]}
+                                alt={`Logo ${index}`}
+                                onClick={() => {
+                                    navigate(`/Stores/${ele.name}`, { state: { sId: ele.storeId } });
+                                }}
+                                className="h-auto w-auto max-h-full max-w-full object-contain cursor-pointer rounded-full"
+                            />
                         </div>
                         <div className="hover:underline text-center group-hover:underline">
                             <span className="flex justify-center gap-2 items-center ">
