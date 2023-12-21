3d36da85a8ccaa02b6205643db75c3ae455d67f1
harshal255 <200280107119@ldceahm.gujgov.edu.in>
2023-12-21 15:04:31 +0530

    update

--- client/src/pages/Store.jsx
+++ client/src/pages/Store.jsx
@@ -770,34 +770,35 @@ const Store = () => {
                     }
                 </div>
             </div>
-            <Dialog open={open} handler={handleOpen} size="lg" className="relative text-black py-5" >
+            <Dialog open={open} handler={handleOpen} size="lg" className="relative text-black p-5" >
                 <IoMdClose className="text-black h-6 w-6 absolute right-5 top-5 cursor-pointer" onClick={() => handleClose()} />
                 <div className="flex flex-col items-center" onClick={handleInsideClick}>
-                    <div className="h-3/4  flex flex-col gap-5 items-center">
+                    <div className="h-3/4  flex flex-col gap-3 lg:gap-5 items-center">
                         <div className="h-[150px] w-[150px] bg-white rounded-full flex items-center justify-center  mt-5 mx-auto border border-black">
                             <img src={str?.logo_url} alt="logo" className='h-auto w-auto px-5' />
                         </div>
-                        <div className="flex gap-5 justify-center items-center"> <div className="text-2xl font-bold">{str?.name.toUpperCase()}</div>
+                        <div className="flex flex-col lg:flex-row gap-5 justify-center items-center">
+                            <div className="text-2xl font-bold">{str?.name.toUpperCase()}</div>
                             <div className="text-2xl font-bold text-black">{selectedProduct.title}</div></div>
                         <div className="text-lg">Ends {formatDate(selectedProduct.due_date)}</div>
                         <div className="flex gap-10 items-center justify-center border border-black rounded-full text-2xl pl-10 p-2 bg-red-50/40">
                             <span className="copy-text tracking-widest">{selectedProduct.coupon_code}</span>
                             <button
-                                className="bg-[#800000]  max-w-fit p-5 text-white cursor-pointer whitespace-nowrap hover:shadow-xl rounded-full"
+                                className="bg-[#800000]  max-w-fit p-2 lg:p-5 text-white cursor-pointer whitespace-nowrap hover:shadow-xl rounded-full"
                                 onClick={handleCopyClick}
                             >
                                 Copy
                             </button>
                         </div>
                         {copySuccess && <span style={{ color: 'green' }}>Copied!</span>}
-                        <div className="text-lg">
+                        <div className="text:sm lg:text-lg">
                             Copy and paste this code at {""}
                             <a href={correctedRefLink} target="_blank" onClick={() => { handleUse(selectedProduct.coupon_id) }} rel="noopener noreferrer" className="underline text-[#800000] hover:cursor-pointer">
                                 {str?.name}
                             </a>
                         </div>
-                        <div className="flex gap-5 border border-[#800000] px-5 py-3 rounded-full items-center justify-center bg-red-50/40">
-                            <span className="text-sm lg:text-lg text-[#800000]">
+                        <div className="flex gap-2 lg:gap-5 border border-[#800000] px-5 py-3 rounded-full items-center justify-center bg-red-50/40">
+                            <span className="text-sm lg:text-lg text-[#800000] whitespace-nowrap">
                                 Did the coupon work?
                             </span>
                             <button className="border border-green-600 hover:bg-green-600 text-black hover:text-white duration-150 px-5 py-3 rounded-md focus:outline-none">
