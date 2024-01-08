310875e3e836a70f58cb814dcbf038978f5c3a39
harshal255 <200280107119@ldceahm.gujgov.edu.in>
2024-01-08 12:32:23 +0530

    advertise page ✨

---
+++ client/src/api/advertise.js
@@ -0,0 +1,29 @@
+const advertise = [
+    {
+        id: 1,
+        text: "Reach a vast audience",
+        content: "Benefit from exposure to our diverse and quality customer base.",
+    },
+    {
+        id: 2,
+        text: "Boost your brand",
+        content: "Increase visibility by placing your banners on our high-traffic website.",
+    },
+    {
+        id: 3,
+        text: "Multiple Traffic Sources",
+        content: "Qwik Savings offers you access to multiple traffic sources, ensuring a diverse and widespread outreach for your brand.",
+    },
+    {
+        id: 4,
+        text: "Exclusive Offers at Low Commission Rates",
+        content: "Benefit from exclusive offers at low commission rates, maximizing your ROI and increasing sales.",
+    },
+    {
+        id: 5,
+        text: "Targeted Customer Outreach",
+        content: "Our targeted approach ensures that your brand reaches the right audience, facilitating a significant boost in sales.",
+    },
+];
+
+export default advertise;
