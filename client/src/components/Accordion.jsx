856d16e9ef6fe0afb35e1820cf2d5e8fbb57d6d6
harshal255 <200280107119@ldceahm.gujgov.edu.in>
2024-01-19 10:04:41 +0530

    faqs & accordion responsive ✨✨

--- client/src/components/Accordion.jsx
+++ client/src/components/Accordion.jsx
@@ -2,15 +2,15 @@ import { FaArrowDown } from "react-icons/fa";
 
 const Accordion = ({ q, a }) => {
     return (
-        <div className="w-[77.5rem] border-b-2 py-10">
-            <details className="group">
-                <summary className="flex cursor-pointer list-none items-center justify-between font-medium">
-                    <li className="font-semibold text-2xl"> {q}</li>
+        <div className="w-[308px] lg:w-[77.5rem] border-b-2 py-5 lg:py-10">
+            <details className="group w-full">
+                <summary className="cursor-pointer list-none items-center gap-5 justify-between font-medium flex">
+                    <li className="font-semibold text-lg lg:text-2xl"> {q}</li>
                     <span className="transition group-open:rotate-180 text-2xl font-light">
                         <FaArrowDown></FaArrowDown>
                     </span>
                 </summary>
-                <p className="group-open:animate-fadeIn mt-5 text-neutral-600">{a}
+                <p className="group-open:animate-fadeIn mt-5 text-neutral-600 text-justify">{a}
                 </p>
             </details>
         </div>
