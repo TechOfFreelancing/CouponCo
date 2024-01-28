da5f0f9015910e7472001755f3000edb16f44cd0
Ridham <115993280+rythm01@users.noreply.github.com>
2024-01-28 22:55:32 +0530

    Update category and event integration done

--- client/src/components/Admin/updateCategory.jsx
+++ client/src/components/Admin/updateCategory.jsx
@@ -1,8 +1,17 @@
-import { Formik, Form, Field } from "formik";
+import { useFormik } from "formik";
 import { toast, Toaster } from 'react-hot-toast'
+import { Avatar } from "@material-tailwind/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 
 function UpdateCategory() {
+
+    const [category, setCategory] = useState([])
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const location = useLocation()
 
     const inputStyle = {
         width: '100%',
@@ -13,84 +22,161 @@ function UpdateCategory() {
         outline: 'none',
     };
 
+    const cId = location.state?.cId;
+
+    const formik = useFormik({
+
+        initialValues: {
+            name: category.name || '',
+            About: category.About || '',
+            todays_top: category.todays_top || '',
+        },
+
+        onSubmit: async (values) => {
+            try {
+
+                console.log(values.todays_top)
+                const formData = new FormData();
+                formData.append("name", values.name);
+                formData.append("todays_top", values.todays_top);
+                formData.append("About", values.About);
+
+                if (selectedFile) {
+                    formData.append("categoryFile", selectedFile);
+                }
+
+                const response = await axios.put(
+                    `${import.meta.env.VITE_SERVER}/api/admin/updateCategory/${cId}`,
+                    formData,
+                    {
+                        headers: {
+                            "Content-Type": "multipart/form-data",
+                            "Authorization": `Bearer ${localStorage.getItem('token')}`
+                        },
+                    }
+                );
+                toast.success("Category Updated successfully");
+                console.log(response.data);
+            } catch (error) {
+                if (error.response) {
+                    toast.error(error.response.data.message);
+                    console.error(error.response.data);
+                } else {
+                    toast.error("An error occurred. Please try again later.");
+                    console.error(error);
+                }
+            }
+        }
+    });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`${import.meta.env.VITE_SERVER}/api/getCategory/${cId}`);
+                console.log(response.data)
+                setCategory(response.data.category);
+
+                formik.setValues({
+                    name: response.data.category.name || '',
+                    About: response.data.category.About || '',
+                    todays_top: response.data.category.todays_top || '',
+                });
+
+            } catch (error) {
+                toast.error(error.response ? error.response.statusText : 'Failed to fetch data');
+                console.error('Error Occurred!', error);
+            }
+        };
+
+        fetchData();
+    }, [cId]);
+
 
     return (
         <>
             <Toaster position="top-center" />
             <div className="max-w-md mx-auto p-4 bg-white rounded-lg">
-                <h1 className="text-center mb-6 text-2xl font-bold">Add New Category</h1>
-                <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-                    <Form>
-                        <div className="my-5">
-                            <label
-                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                                htmlFor="storeFile"
-                            >
-                                Upload image:
-                            </label>
-                            <input
-                                id="storeFile"
-                                type="file"
-                                style={inputStyle}
-                                className="w-full p-2 border rounded"
-                                onChange={(event) => setSelectedFile(event.target.files[0])}
-                            />
-                        </div>
-
-                        <div className="mb-4">
-                            <label htmlFor="name" className="block mb-1 font-medium">
-                                Name:
-                            </label>
-                            <Field
-                                type="text"
-                                id="name"
-                                name="name"
-                                style={inputStyle}
-                                className="w-full p-2 border rounded"
-                            />
-                        </div>
-
-                        <div className="mb-4">
-                            <label htmlFor="todays_top" className="block mb-1 font-medium">
-                                Type:
-                            </label>
-                            <Field
-                                as="select"
-                                id="todays_top"
-                                name="todays_top"
-                                style={inputStyle}
-                            >
-                                <option value="">Select Option</option>
-                                <option value="1">
-                                    Yes
-                                </option>
-                                <option value="0">
-                                    No
-                                </option>
-                            </Field>
-                        </div>
-
-                        <div className="mb-4">
-                            <label htmlFor="description" className="block mb-1 font-medium">
-                                About:
-                            </label>
-                            <Field
-                                as="textarea"
-                                id="description"
-                                name="description"
-                                style={inputStyle}
-                                className="w-full p-2 border rounded resize-vertical"
-                                rows={4}
-                            />
-                        </div>
-                        <button
-                            type="submit"
-                            className="w-full py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:ring focus:ring-pink-200"
+                <h1 className="text-center mb-6 text-2xl font-bold">Update Category</h1>
+                <div className="flex items-center justify-center">
+                    {category && <Avatar src={category?.logo_url} size="xxl" className="h-auto w-[150px]" alt="avatar"></Avatar>}
+                </div>
+                <form onSubmit={formik.handleSubmit}>
+                    <div className="my-5">
+                        <label
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            htmlFor="categoryFile"
                         >
-                            Submit
-                        </button>
-                    </Form>
-                </Formik>
+                            Upload image:
+                        </label>
+                        <input
+                            id="categoryFile"
+                            type="file"
+                            style={inputStyle}
+                            className="w-full p-2 border rounded"
+                            onChange={(event) => setSelectedFile(event.target.files[0])}
+                        />
+                    </div>
+
+                    <div className="mb-4">
+                        <label htmlFor="name" className="block mb-1 font-medium">
+                            Name:
+                        </label>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            style={inputStyle}
+                            value={formik.values.name}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            className="w-full p-2 border rounded"
+                        />
+                    </div>
+
+                    <div className="mb-4">
+                        <label htmlFor="todays_top" className="block mb-1 font-medium">
+                            Add This Category to Today's Top?:
+                        </label>
+                        <select
+                            id="todays_top"
+                            name="todays_top"
+                            style={inputStyle}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.todays_top}
+                        >
+                            <option value="">Select Option</option>
+                            <option value="1">
+                                Yes
+                            </option>
+                            <option value="0">
+                                No
+                            </option>
+                        </select>
+                    </div>
+
+                    <div className="mb-4">
+                        <label htmlFor="About" className="block mb-1 font-medium">
+                            About:
+                        </label>
+                        <textarea
+                            id="About"
+                            name="About"
+                            style={inputStyle}
+                            onChange={formik.handleChange}
+                            value={formik.values.About}
+                            onBlur={formik.handleBlur}
+                            className="w-full p-2 border rounded resize-vertical"
+                            rows={4}
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="w-full py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:ring focus:ring-pink-200"
+                    >
+                        Submit
+                    </button>
+                </form>
             </div>
         </>
     );
--- client/src/components/Admin/updateEvent.jsx
+++ client/src/components/Admin/updateEvent.jsx
@@ -1,11 +1,16 @@
-import { Formik, Form, Field } from "formik";
+import { useFormik } from "formik";
 import { toast, Toaster } from 'react-hot-toast'
+import { Avatar } from "@material-tailwind/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 
 function UpdateEvent() {
 
     const [selectedFile, setSelectedFile] = useState(null);
     const [selectedBannerFile, setSelectedBannerFile] = useState(null);
+    const [event, setEvent] = useState([])
 
     const inputStyle = {
         width: '100%',
@@ -16,124 +21,198 @@ function UpdateEvent() {
         outline: 'none',
     };
 
+    const location = useLocation()
+
+    const eId = location.state?.eId
+
+    const formik = useFormik({
+
+        initialValues: {
+            event_name: event.event_name || '',
+            best_offer: event.best_offer || '',
+            avg_disc: event.avg_disc || '',
+            about: event.about || '',
+        },
+
+        onSubmit: async (values) => {
+            try {
+                const formData = new FormData();
+                formData.append("event_name", values.event_name);
+                formData.append("best_offer", values.best_offer);
+                formData.append("avg_disc", values.avg_disc);
+                formData.append("about", values.about);
+
+                if (selectedFile) {
+                    formData.append("eventLogoFile", selectedFile);
+                }
+
+                if (selectedBannerFile) {
+                    formData.append("eventBannerFile", selectedBannerFile);
+                }
+
+                const response = await axios.put(
+                    `${import.meta.env.VITE_SERVER}/api/admin/updateEvent/${eId}`,
+                    formData,
+                    {
+                        headers: {
+                            "Content-Type": "multipart/form-data",
+                            "Authorization": `Bearer ${localStorage.getItem('token')}`
+                        },
+                    }
+                );
+                toast.success("Event Updated successfully");
+                console.log(response.data);
+            } catch (error) {
+                if (error.response) {
+                    toast.error(error.response.data.message);
+                    console.error(error.response.data);
+                } else {
+                    toast.error("An error occurred. Please try again later.");
+                    console.error(error);
+                }
+            }
+        }
+    });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`${import.meta.env.VITE_SERVER}/api/getEvent/${eId}`);
+                setEvent(response.data.data)
+
+                formik.setValues({
+                    event_name: response.data.data.event_name || '',
+                    best_offer: response.data.data.best_offer || '',
+                    avg_disc: response.data.data.avg_disc || '',
+                    about: response.data.data.about || '',
+                });
+
+            } catch (error) {
+                toast.error(error.response ? error.response.statusText : 'Failed to fetch data');
+                console.error('Error Occurred!', error);
+            }
+        };
+
+        fetchData();
+    }, [eId]);
+
 
     return (
         <>
             <Toaster position="top-center"></Toaster>
             <div className="max-w-md mx-auto p-4 bg-white rounded-lg">
                 <h1 className="text-center mb-6 text-2xl font-bold">Update Event</h1>
-                <Formik>
-                    <Form>
-                        <div className="my-5">
-                            <label
-                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                                htmlFor="storeFile"
-                            >
-                                Upload image:
-                            </label>
-                            <input
-                                id="storeFile"
-                                type="file"
-                                style={inputStyle}
-                                onChange={(event) => setSelectedFile(event.target.files[0])}
-                            />
-                        </div>
-                        <div className="my-5">
-                            <label
-                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                                htmlFor="storeFile"
-                            >
-                                Upload Banner image:
-                            </label>
-                            <input
-                                id="storeFile"
-                                type="file"
-                                style={inputStyle}
-                                onChange={(event) => setSelectedBannerFile(event.target.files[0])}
-                            />
-                        </div>
-
-                        <div className="mb-4">
-                            <label htmlFor="name" className="block mb-1 font-medium">
-                                Name:
-                            </label>
-                            <Field
-                                type="text"
-                                id="name"
-                                name="name"
-                                style={inputStyle}
-                            />
-                        </div>
-
-                        <div className="mb-4">
-                            <label htmlFor="title" className="block mb-1 font-medium">
-                                title:
-                            </label>
-                            <Field
-                                type="text"
-                                id="title"
-                                name="title"
-                                style={inputStyle}
-                            />
-                        </div>
-
-
-
-                        <div className="mb-4">
-                            <label htmlFor="description" className="block mb-1 font-medium">
-                                About:
-                            </label>
-                            <Field
-                                as="textarea"
-                                id="description"
-                                name="description"
-                                style={{
-                                    width: '100%',
-                                    padding: '0.75rem',
-                                    border: '1px solid black',
-                                    borderRadius: '0.375rem',
-                                    boxShadow: '0 1px 2px rgba(0, 0, 0, 0.05)',
-                                    outline: 'none',
-                                    resize: 'vertical',
-                                }}
-                                rows={4}
-                            />
-                        </div>
-
-
-
-                        <div className="mb-4">
-                            <label htmlFor="best_offer" className="block mb-1 font-medium">
-                                Best Offer(%):
-                            </label>
-                            <Field
-                                id="best_offer"
-                                name="best_offer"
-                                style={inputStyle}
-                            />
-                        </div>
-
-                        <div className="mb-4">
-                            <label htmlFor="avg_disc" className="block mb-1 font-medium">
-                                Avg Discout(%):
-                            </label>
-                            <Field
-                                id="avg_disc"
-                                name="avg_disc"
-                                style={inputStyle}
-                            />
-                        </div>
-
-
-
-                        <button
-                            type="submit"
-                            className="w-full py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:ring focus:ring-pink-200"
+                <div className="flex items-center justify-center">
+                    <h1>Logo:</h1>
+                    {event && <Avatar src={event?.event_logo_url} size="xxl" className="h-auto w-[150px]" alt="avatar"></Avatar>}
+                </div>
+                <div className="flex items-center justify-center">
+                    <h1>Banner:</h1>
+                    {event && <Avatar src={event?.event_banner_url} size="xxl" className="h-auto w-[150px]" alt="avatar"></Avatar>}
+                </div>
+                <form onSubmit={formik.handleSubmit}>
+                    <div className="my-5">
+                        <label
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            htmlFor="eventLogoFile"
                         >
-                            Submit
-                        </button>
-                    </Form>
-                </Formik>
+                            Upload logo image:
+                        </label>
+                        <input
+                            id="eventLogoFile"
+                            type="file"
+                            style={inputStyle}
+                            onChange={(event) => setSelectedFile(event.target.files[0])}
+                        />
+                    </div>
+                    <div className="my-5">
+                        <label
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            htmlFor="eventBannerFile"
+                        >
+                            Upload Banner image:
+                        </label>
+                        <input
+                            id="eventBannerFile"
+                            type="file"
+                            style={inputStyle}
+                            onChange={(event) => setSelectedBannerFile(event.target.files[0])}
+                        />
+                    </div>
+
+                    <div className="mb-4">
+                        <label htmlFor="event_name" className="block mb-1 font-medium">
+                            Event Name:
+                        </label>
+                        <input
+                            type="text"
+                            id="event_name"
+                            name="event_name"
+                            value={formik.values.event_name}
+                            style={inputStyle}
+                            onBlur={formik.handleBlur}
+                            onChange={formik.handleChange}
+                        />
+                    </div>
+
+                    <div className="mb-4">
+                        <label htmlFor="about" className="block mb-1 font-medium">
+                            about:
+                        </label>
+                        <textarea
+                            id="about"
+                            name="about"
+                            value={formik.values.about}
+                            style={{
+                                width: '100%',
+                                padding: '0.75rem',
+                                border: '1px solid black',
+                                borderRadius: '0.375rem',
+                                boxShadow: '0 1px 2px rgba(0, 0, 0, 0.05)',
+                                outline: 'none',
+                                resize: 'vertical',
+                            }}
+                            rows={4}
+                            onBlur={formik.handleBlur}
+                            onChange={formik.handleChange}
+                        />
+                    </div>
+
+                    <div className="mb-4">
+                        <label htmlFor="best_offer" className="block mb-1 font-medium">
+                            Best Offer(%):
+                        </label>
+                        <input
+                            id="best_offer"
+                            name="best_offer"
+                            value={formik.values.best_offer}
+                            style={inputStyle}
+                            onBlur={formik.handleBlur}
+                            onChange={formik.handleChange}
+                        />
+                    </div>
+
+                    <div className="mb-4">
+                        <label htmlFor="avg_disc" className="block mb-1 font-medium">
+                            Avg Discout(%):
+                        </label>
+                        <input
+                            id="avg_disc"
+                            name="avg_disc"
+                            value={formik.values.avg_disc}
+                            style={inputStyle}
+                            onBlur={formik.handleBlur}
+                            onChange={formik.handleChange}
+                        />
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="w-full py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:ring focus:ring-pink-200"
+                    >
+                        Submit
+                    </button>
+                </form>
             </div>
         </>
     );
